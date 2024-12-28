b4bb7426abf4b6c2aae195d81439f0d192d48abf
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-12-28 18:06:05 +0100

    Add patients and practitioners attributes to condition form

--- ndb_rest_api/lib/ndb_rest_api_web/controllers/condition_html.ex
+++ ndb_rest_api/lib/ndb_rest_api_web/controllers/condition_html.ex
@@ -7,6 +7,8 @@ defmodule NdbRestApiWeb.ConditionHTML do
   Renders a condition form.
   """
   attr :changeset, Ecto.Changeset, required: true
+  attr :patients, :list, required: true
+  attr :practitioners, :list, required: true
   attr :action, :string, required: true
 
   def condition_form(assigns)
