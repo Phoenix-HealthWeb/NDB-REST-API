3ed84b0cb2d09075056b9a35dfecf24e150432da
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-12-28 18:09:29 +0100

    Add patients and practitioners attributes to observation form

--- ndb_rest_api/lib/ndb_rest_api_web/controllers/observation_html.ex
+++ ndb_rest_api/lib/ndb_rest_api_web/controllers/observation_html.ex
@@ -4,9 +4,11 @@ defmodule NdbRestApiWeb.ObservationHTML do
   embed_templates "observation_html/*"
 
   @doc """
-  Renders a observation form.
+  Renders an observation form.
   """
   attr :changeset, Ecto.Changeset, required: true
+  attr :patients, :list, required: true
+  attr :practitioners, :list, required: true
   attr :action, :string, required: true
 
   def observation_form(assigns)
