456f19ad0191161b472968d037472a0297a9e079
Francesco Forcellato <francesco.forcellato@gmail.com>
2025-01-09 12:15:28 +0100

    Refactor reset_db task to ensure data deletion and insertion occur in the correct order; update CSV headers for admins

--- ndb_rest_api/lib/mix/tasks/reset_db.ex
+++ ndb_rest_api/lib/mix/tasks/reset_db.ex
@@ -16,20 +16,20 @@ defmodule Mix.Tasks.ResetDb do
   def run(_) do
     Mix.Task.run("app.start")
 
-    # Delete all data
+    # Delete all data in the correct order
     Repo.delete_all(HospitalsPractitioners)
-    Repo.delete_all(Hospital)
     Repo.delete_all(Practitioner)
     Repo.delete_all(PractitionerRole)
+    Repo.delete_all(Hospital)
     Repo.delete_all(Patient)
     Repo.delete_all(Gender)
     Repo.delete_all(Admin)
 
-    # Insert default data from CSV files
-    insert_admin()
+    # Insert default data from CSV files in the correct order
     insert_genders()
-    insert_patients()
     insert_practitioner_roles()
+    insert_admin()
+    insert_patients()
     insert_practitioners()
     insert_hospitals()
     insert_hospitals_practitioners()
@@ -41,8 +41,9 @@ defmodule Mix.Tasks.ResetDb do
     "priv/repo/seeds/admins.csv"
     |> File.stream!()
     |> CSV.decode(headers: true)
-    |> Enum.each(fn {:ok, %{"username" => email, "password" => password}} ->
-      %Admin{email: email, hashed_password: hash_pwd_salt(password)}
+    |> Enum.with_index(1)
+    |> Enum.each(fn {{:ok, %{"email" => email, "password" => password}}, index} ->
+      %Admin{id: index, email: email, hashed_password: hash_pwd_salt(password)}
       |> Repo.insert!()
     end)
   end
@@ -51,8 +52,9 @@ defmodule Mix.Tasks.ResetDb do
     "priv/repo/seeds/genders.csv"
     |> File.stream!()
     |> CSV.decode(headers: true)
-    |> Enum.each(fn {:ok, %{"id" => id, "name" => name}} ->
-      %Gender{id: String.to_integer(id), name: name}
+    |> Enum.with_index(1)
+    |> Enum.each(fn {{:ok, %{"name" => name}}, index} ->
+      %Gender{id: index, name: name}
       |> Repo.insert!()
     end)
   end
@@ -61,8 +63,10 @@ defmodule Mix.Tasks.ResetDb do
     "priv/repo/seeds/patients.csv"
     |> File.stream!()
     |> CSV.decode(headers: true)
-    |> Enum.each(fn {:ok, %{"firstname" => firstname, "lastname" => lastname, "cf" => cf, "date_of_birth" => date_of_birth, "gender_id" => gender_id}} ->
+    |> Enum.with_index(1)
+    |> Enum.each(fn {{:ok, %{"firstname" => firstname, "lastname" => lastname, "cf" => cf, "date_of_birth" => date_of_birth, "gender_id" => gender_id}}, index} ->
       %Patient{
+        id: index,
         firstname: firstname,
         lastname: lastname,
         cf: cf,
@@ -77,8 +81,9 @@ defmodule Mix.Tasks.ResetDb do
     "priv/repo/seeds/practitioner_roles.csv"
     |> File.stream!()
     |> CSV.decode(headers: true)
-    |> Enum.each(fn {:ok, %{"name" => name}} ->
-      %PractitionerRole{name: name}
+    |> Enum.with_index(1)
+    |> Enum.each(fn {{:ok, %{"name" => name}}, index} ->
+      %PractitionerRole{id: index, name: name}
       |> Repo.insert!()
     end)
   end
@@ -87,8 +92,10 @@ defmodule Mix.Tasks.ResetDb do
     "priv/repo/seeds/practitioners.csv"
     |> File.stream!()
     |> CSV.decode(headers: true)
-    |> Enum.each(fn {:ok, %{"forename" => forename, "surname" => surname, "email" => email, "gender_id" => gender_id, "role_id" => role_id, "date_of_birth" => date_of_birth, "qualification" => qualification}} ->
+    |> Enum.with_index(1)
+    |> Enum.each(fn {{:ok, %{"forename" => forename, "surname" => surname, "email" => email, "gender_id" => gender_id, "role_id" => role_id, "date_of_birth" => date_of_birth, "qualification" => qualification}}, index} ->
       %Practitioner{
+        id: index,
         forename: forename,
         surname: surname,
         email: email,
@@ -105,8 +112,10 @@ defmodule Mix.Tasks.ResetDb do
     "priv/repo/seeds/hospitals.csv"
     |> File.stream!()
     |> CSV.decode(headers: true)
-    |> Enum.each(fn {:ok, %{"name" => name, "address" => address, "region" => region, "notes" => notes}} ->
+    |> Enum.with_index(1)
+    |> Enum.each(fn {{:ok, %{"name" => name, "address" => address, "region" => region, "notes" => notes}}, index} ->
       %Hospital{
+        id: index,
         name: name,
         address: address,
         region: region,
